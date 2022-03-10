0556e811a8d7f3a176e37d94bce4039e7f5e40c5
João Thallis <joaothallis@icloud.com>
2022-03-10 10:19:45 -0300

    Setup create fzf result quickfix

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,6 +9,22 @@ require('plugins')
 vim.opt.termguicolors = true
 vim.api.nvim_command 'colorscheme rvcs'
 
+vim.cmd([[
+function! s:build_quickfix_list(lines)
+  call setqflist(map(copy(a:lines), '{ "filename": v:val }'))
+  copen
+  cc
+endfunction
+
+let g:fzf_action = {
+  \ 'ctrl-q': function('s:build_quickfix_list'),
+  \ 'ctrl-t': 'tab split',
+  \ 'ctrl-x': 'split',
+  \ 'ctrl-v': 'vsplit' }
+
+let $FZF_DEFAULT_OPTS = '--bind ctrl-a:select-all'
+]])
+
 vim.api.nvim_set_keymap('n', '<Leader>ff', ":GitFiles<CR>", {noremap = true})
 
 local opts = {noremap = true, silent = true}
