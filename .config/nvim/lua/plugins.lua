d08fee1bf6ec66416d4bf24e5ee9354c00c92ee4
João Thallis <joaothallis@icloud.com>
2022-03-24 16:59:55 -0300

    Remove vscode.nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,8 +1,6 @@
 return require('packer').startup(function()
     use 'wbthomason/packer.nvim'
 
-    use 'Mofiqul/vscode.nvim'
-
     use 'shaeinst/roshnivim-cs'
 
     use {'tyru/open-browser-github.vim', requires = {{'tyru/open-browser.vim'}}}
