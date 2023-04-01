81e4aaeb733a0b178166742b098b261b2aaddd82
João Thallis <joaothallis@icloud.com>
2023-04-01 16:00:03 -0300

    `tmux_new` for inside tmux session

--- .config/fish/functions/tmux_new.fish
+++ .config/fish/functions/tmux_new.fish
@@ -1,3 +1,8 @@
 function tmux_new
-    tmux new -t (basename (pwd))
+    if test -n "$TMUX"
+        tmux new -s (basename (pwd)) -d
+        tmux switch-client -t (basename (pwd))
+    else
+        tmux new -t (basename (pwd))
+    end
 end
