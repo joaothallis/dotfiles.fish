1d334286c92c5a4e86757b21d74123b144c13ff7
João Thallis <joaothallis@icloud.com>
2023-07-17 01:44:51 -0300

    Fix create tmux session with names with .

--- .config/fish/functions/tmux_new.fish
+++ .config/fish/functions/tmux_new.fish
@@ -1,8 +1,10 @@
 function tmux_new
+    set session (basename (pwd))
     if test -n "$TMUX"
-        tmux new -s (basename (pwd)) -d
-        tmux switch-client -t (basename (pwd))
+        tmux new -s $session -d
+        set session (string replace -a . _ $session)
+        tmux switch-client -t $session
     else
-        tmux new -t (basename (pwd))
+        tmux new -t $session
     end
 end
