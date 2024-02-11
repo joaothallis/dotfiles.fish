12b9ceae76b7be45ded14547329a6d73ff0c9477
João Thallis <joaothallis@icloud.com>
2024-02-11 11:33:25 -0300

    Format fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -66,6 +66,6 @@ $HOME/.local/bin/mise activate fish | source
 # pnpm
 set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
